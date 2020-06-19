964dfd89bb19257ade3d59a386b0107401ab112a
Pietrzi <pietrzi@op.pl>
2020-06-19 18:53:51 +0200

    basic mobile and desktop version with non animated but working navigation

--- dist/app.js
+++ dist/app.js
@@ -11,11 +11,24 @@ $(function() {
     });
 });
 
+
+
+const menuButton = document.querySelector('.toggle-button');
+const menuButtonContainer = document.querySelector('.mobile-button-container');
 const mobileMenu = document.querySelector('.mobile-nav-wrapper');
 const mobileMenuNav = document.querySelector('.mobile-nav');
 
+const openMobileMenu = () => {
+    mobileMenu.style.display = "flex";
+    menuButton.style.backgroundColor = "#ffaa80";
+    menuButtonContainer.style.backgroundColor = "#ffaa80";
+}
+
 const closeMobileMenu = () => {
     mobileMenu.style.display = "none";
+    menuButton.style.backgroundColor = "white";
+    menuButtonContainer.style.backgroundColor = "white";
 }
 
 mobileMenuNav.addEventListener('click', closeMobileMenu);
+menuButton.addEventListener('click', openMobileMenu);
